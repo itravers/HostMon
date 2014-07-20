0c8e8615f6d3e89874dd05b5c010b35546860756
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-07-19 23:39:12 -0700

    more notes

--- hostmon2/DefaultGateway.java
+++ hostmon2/DefaultGateway.java
@@ -1,5 +1,5 @@
 //to send to linux, commit, and push to remote.
-//then go to /home/slack/hostmon and fetch.
+//then go to /home/slack/hostmon and fetch. then git merge --no-ff origin/master master
 //to send to windows, commit, and push to remote.
 //go to team, fetch from remote
 import java.io.*;
