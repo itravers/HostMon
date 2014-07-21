984e71bd88637802734e62d9a2122e1561ddbe96
Isaac Assegai <isaac.a.travers@gmail.com>
2014-07-21 16:27:51 -0700

    we should now be able to mark a runnable as active=false. The next time it is run PingThread will check and make sure it's active, or it won't re add it to the queue, it'll be dropped and garbage collected.

--- hostmon2/PingThread.java
+++ hostmon2/PingThread.java
@@ -1,40 +1,75 @@
 import java.util.concurrent.PriorityBlockingQueue;
 
+/**
+ * This is a Thread in the Thread Pool. Each Thread will take a Runnable from
+ * the queue and run it. After the thread runs, it will first find the amount of
+ * time it took the runnable to complete, and add that to it's own total time.
+ * It will track how many times it has run. It will give us an average time it
+ * takes this thread to run a job. After it does this, it will check the
+ * runnable to see if it is supposed to be run again, if so it will requeue it.
+ * If not it will destroy it.
+ * 
+ * @author Isaac Assegai
+ * 
+ */
+public class PingThread extends Thread {
 
-public class PingThread extends Thread{
-	
-	 
+	public PingThread(PriorityBlockingQueue<RunnablePing> queue) {
+		totalRunTime = 0;
+		numRuns = 0;
+		this.queue = queue;
+		isStopped = false;
+	}
 
-	  public PingThread(PriorityBlockingQueue<RunnablePing> queue){
-	    this.queue = queue;
-	    isStopped = false;
-	  }
+	public void run() {
+		while (!isStopped) {
+			try {
+				RunnablePing runnable = (RunnablePing) queue.take();
+				runnable.run();
+				
+				// update numRuns and totalRunTime counts;
+				totalRunTime += ((RunnablePing)runnable).getRunTime();
+				numRuns++;
+				
+				// check if runnable is still active, requeue if so, destroy if not
+				if(((RunnablePing)runnable).active){
+					queue.add(runnable);
+				}else{
+					//it'll get garbage collected by the jvm
+				}
+			} catch (Exception e) {
+				System.out.println(e);
+				// log or otherwise report exception,
+				// but keep pool thread alive.
+			}
+			
+			
+		}
+	}
 
-	  public void run(){
-	    while(!isStopped){
-	      try{
-	        Runnable runnable = (Runnable) queue.take();
-	        runnable.run();
-	      } catch(Exception e){
-	        //log or otherwise report exception,
-	        //but keep pool thread alive.
-	      }
-	    }
-	  }
-
-	  public synchronized void stopThread(){
+	public synchronized void stopThread() {
 		stop();
-	    isStopped = true;
-	    this.interrupt(); //break pool thread out of dequeue() call.
-	  }
+		isStopped = true;
+		this.interrupt(); // break pool thread out of dequeue() call.
+	}
 
-	  public synchronized boolean isStopped(){
-	    return isStopped;
-	  }
+	public synchronized boolean isStopped() {
+		return isStopped;
+	}
 	
+	public long getAverageRunTime(){
+		return totalRunTime / numRuns;
+	}
+	
+	public void resetAverageRunTime(){
+		totalRunTime = 0;
+		numRuns = 0;
+	}
+
 	/* Field Objects & Variables */
 	private PriorityBlockingQueue<RunnablePing> queue;
 	private boolean isStopped;
-	
+	private long totalRunTime;
+	private int numRuns;
 
 }
