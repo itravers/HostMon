eeb7a5cf9effcdcf1f01fe4be6ab98e3673d8443
Isaac Assegai <isaac.a.travers@gmail.com>
2014-07-21 14:10:16 -0700

    Started the DataBase file, getting hostmon further along.

---
+++ hostmon2/DataBase.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+/**
+ * Class Responsible for all DataBase Connectivity.
+ * @author Isaac Assegai
+ *
+ */
+public class DataBase {
+	
+	/**
+	 * Constructor
+	 * @param options - Array List of options used for connecting and reading from db.
+	 */
+	public DataBase(ArrayList<String>options){
+		this.options = options;
+	}
+	
+	/* Public Methods*/
+	
+	/**
+	 * Opens the db for reading or writing.
+	 * @return True if db is now open. Else, False.
+	 */
+	public boolean open(){
+		return false;
+	}
+	
+	/**
+	 * Lets us know if the db is currently open, or not.
+	 * @return True if Open
+	 */
+	public boolean isOpen(){
+		return false;
+	}
+	
+	/**
+	 * Closes the db.
+	 * @return True if Closed.
+	 */
+	public boolean close(){
+		return false;
+	}
+	
+	/**
+	 * Send a command to make a change in the database.
+	 * @param dbCommand
+	 * @return
+	 */
+	public boolean write(String dbCommand){
+		return false;
+	}
+	
+	/**
+	 * Send a command to pull up information from the db.
+	 * @param dbCommand
+	 * @return
+	 */
+	public String read(String dbCommand){
+		return null;
+	}
+	
+	/*Private Methods*/
+	
+	/*
+	 * Field Objects and Variables
+	 */
+	ArrayList<String>options;
+}
---
+++ hostmon2/Hostmon.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+/**
+ * Java Back-End of the HostMon Project.
+ * This program will retrieve a list of ips to ping from a database.
+ * It will then implement a strategy to ping each ip within an allotted time.
+ * @author Isaac Assegai
+ *
+ */
+public class Hostmon {
+
+	/**
+	 * The Program Start
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hostmon hostmon = new Hostmon();
+	}
+	
+	/**
+	 * The Constructor.
+	 */
+	public Hostmon(){
+		//Initialize Field Objects & Variables
+		init(); 
+	}
+	
+	/*
+	 * Public Methods
+	 */
+	
+	/*
+	 * Private Methods
+	 */
+	
+	private void init(){
+		//Retrieve the database connection options.
+		ArrayList <String> dbOptions = Functions.getDBOptions();
+				
+		//Instantiate the database object we will use to talk to the db.
+		db = new DataBase(dbOptions);
+		
+		//Instantiate the class that will be responsible for maintaining the db.
+		dbMaintainer = new DBMaintainer(db);
+		
+		lChecker = new LatencyChecker(db);
+	}
+	
+	/*
+	 * Field Objects & Variables
+	 */
+	
+	/** The DataBase Object */
+	private DataBase db;
+	
+	/** The DataBase Maintainer */
+	private DBMaintainer dbMaintainer;
+	
+	/** The Main Latency Checker */
+	private LatencyChecker lChecker;
+}
