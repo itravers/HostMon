fb957d82e5d36cb0309b6329d0440de71d4f5ebb
Isaac Assegai <isaac.a.travers@gmail.com>
2014-08-18 16:37:19 -0700

    stepping towards getting grid-backend working.

--- php/grid-backend.php
+++ php/grid-backend.php
@@ -3,6 +3,44 @@
 	include_once("db.php");
 	$postResult = "";
 	$postResult = $_POST;
-	echo print_r($postResult);
+	
+	//deviceName
+	if($_POST['addNewDevice']){
+		//$postResult = $postResult.$_POST['addNewDevice'];
+		/*first we check the db to see if a device of this ip has already been added
+		  if it has, then we pull the info for that device from the db,
+		  		   , then we make sure the device is in/added to the active devices list in db.
+				   , then we create a render of that device and send it to the frontend.
+		  if it has not been added, then create a new device record with the ip, name, and initial note.
+		  			, then we make sure the new device is in active devices list in db.
+					, then we create a render of that device and send it to the frontend.
+		*/
+		$ip = $_POST['deviceIP'];
+		$name = $_POST['deviceName'];
+		$note = $_POST['deviceNote'];
+		
+		if(deviceExists($ip)){
+			$postResult = " DeviceExists | display";
+		}else{
+			$postResult = " AddedDevice | display";
+		}
+	}
+	
+	function deviceExists($ip){
+		return false;	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	echo $postResult;
 
 ?>
