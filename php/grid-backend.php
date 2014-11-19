6a62bdbc9bdfbbb75167d252fc1513112eb9d410
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-18 17:42:30 -0800

    formatting

--- php/grid-backend.php
+++ php/grid-backend.php
@@ -1,24 +1,24 @@
 <?php
-error_reporting(-1);
-	include_once("functions.php");
-	include_once("db.php");
-	$postResult = "";
-	//$postResult = $_POST;
-	
-	//deviceName
-	if(isset($_POST['addNewDevice'])){
-		//$postResult = $postResult.$_POST['addNewDevice'];
-		/*first we check the db to see if a device of this ip has already been added
-		  if it has, then we pull the info for that device from the db,
-		  		   , then we make sure the device is in/added to the active devices list in db.
-				   , then we create a render of that device and send it to the frontend.
-		  if it has not been added, then create a new device record with the ip, name, and initial note.
-		  			, then we make sure the new device is in active devices list in db.
-					, then we create a render of that device and send it to the frontend.
-		*/
-		//$ip = $_POST['deviceIP'];
-		//$name = $_POST['deviceName'];
-		//$note = $_POST['deviceNote'];
+//error_reporting(-1);
+include_once("functions.php");
+include_once("db.php");
+$postResult = "";
+//$postResult = $_POST;
+
+/** User is trying to add a new device to the system. */
+if(isset($_POST['addNewDevice'])){ //This function is not completely done yet.
+	//$postResult = $postResult.$_POST['addNewDevice'];
+	/*first we check the db to see if a device of this ip has already been added
+	if it has, then we pull the info for that device from the db,
+		, then we make sure the device is in/added to the active devices list in db.
+		, then we create a render of that device and send it to the frontend.
+	if it has not been added, then create a new device record with the ip, name, and initial note.
+		, then we make sure the new device is in active devices list in db.
+		, then we create a render of that device and send it to the frontend.
+	*/
+	//$ip = $_POST['deviceIP'];
+	//$name = $_POST['deviceName'];
+	//$note = $_POST['deviceNote'];
 		
 		$ip = "chicosystems.com";
 		$name = "Chico Systems";
