f490f7e2bdbaeaeee008eb4ab1f55d94f06d80b3
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-20 12:05:01 -0800

    formatting login.php

--- login.php
+++ login.php
@@ -66,18 +66,9 @@ if(isset($_GET['logout'])){ // User is logging out.
 	</div>
 	<div class="ajax-spinner-bars"> <!-- The loading animation displayed on the submit button. -->
 		<div class="bar-1"></div><div class="bar-2"></div><div class="bar-3"></div><div class="bar-4"></div>
-		<div class="bar-5"></div>
-		<div class="bar-6"></div>
-		<div class="bar-7"></div>
-		<div class="bar-8"></div>
-		<div class="bar-9"></div>
-		<div class="bar-10"></div>
-		<div class="bar-11"></div>
-		<div class="bar-12"></div>
-		<div class="bar-13"></div>
-		<div class="bar-14"></div>
-		<div class="bar-15"></div>
-		<div class="bar-16"></div>
+		<div class="bar-5"></div><div class="bar-6"></div><div class="bar-7"></div><div class="bar-8"></div>
+		<div class="bar-9"></div><div class="bar-10"></div><div class="bar-11"></div><div class="bar-12"></div>
+		<div class="bar-13"></div><div class="bar-14"></div><div class="bar-15"></div><div class="bar-16"></div>
 	</div>
 
 
