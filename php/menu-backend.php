cd252e04fc13ffd022f5c1e84b450c06cf5bf861
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-20 17:33:30 -0800

    have db in php array now
    next step is to figure out how to send it back to js in json

--- php/menu-backend.php
+++ php/menu-backend.php
@@ -1,6 +1,28 @@
 <?php 
+include_once("db.php");
+include_once("functions.php");
+session_start();
+$_POST['getConfigData'] = true;
 if(isset($_POST['getConfigData'])){
+	$con = openDB();
+	mysqli_select_db($con,"HostMon");
+	$sql = "SELECT * FROM configuration";
+	$result = mysqli_query($con,$sql);
+	$config = array();
+	$id = array();
+	$row = array();;
+	while($row = mysqli_fetch_array($result)) {
+		foreach($row as  $key => $value){
+			if(!((string)(int)$key == $key)){
+				$id[$key] = $value;
+			}
+			
+		}
+		array_push($config, $id);
+		//unset($value);
+		
+	}
 	
 }
-
+echo print_r($config);
 ?>
