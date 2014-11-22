e2a082becb19f17ead82d6edd246c3cbad4c9ea4
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-22 12:33:57 -0800

    starting install gui

---
+++ install/install.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	<script src="js/jquery.tools.min.js"></script>
+</head>
+<body class='main-body'>
+	<div class='install'>
+		<h1>Hostmon Installation</h1>
+		<h2>PHP Version</h2><h2>5.3</h2>
+	</div>
+</body>
+</html>
