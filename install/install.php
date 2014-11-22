e5526243e4cc0c81ee0445cfdd9f21421f43ded8
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-22 13:35:28 -0800

    table works

--- install/install.php
+++ install/install.php
@@ -1,20 +1,22 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="../css/styles.css">
-	<script src="js/jquery.tools.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/styles.css">
+<script src="js/jquery.tools.min.js"></script>
 </head>
 <body class='main-body'>
 	<h1>Hostmon Installation</h1>
-	<table>
-	<thead>Dependencies</thead>
-  <tr>
-    <td>PHP Version:</td> 
-    <td>5.3</td>
-  </tr>
-  <tr>
-    <td>MySQL Version</td>
-    <td>2.2</td>
-  </tr>
-</table>
+	<div class='install'>
+		<table class='install_table'>
+		<caption>Dependencies</caption>
+			<tr>
+				<td class='install_label'>PHP Version</td>
+				<td class='install_value' id='install_red'>5.3</td>
+			</tr>
+			<tr>
+				<td class='install_label'>MySQL Version</td>
+				<td class='install_value' id='install_green'>5.3</td>
+			</tr>
+		</table>
+	</div>
 </body>
 </html>
