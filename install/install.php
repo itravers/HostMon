b9e985fbfe56c07dc5fdbd836b60266dd85b80c1
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-22 13:12:22 -0800

    changed to tables

--- install/install.php
+++ install/install.php
@@ -4,9 +4,17 @@
 	<script src="js/jquery.tools.min.js"></script>
 </head>
 <body class='main-body'>
-	<div class='install'>
-		<h1>Hostmon Installation</h1>
-		<h2>PHP Version</h2><h2>5.3</h2>
-	</div>
+	<h1>Hostmon Installation</h1>
+	<table>
+	<thead>Dependencies</thead>
+  <tr>
+    <td>PHP Version:</td> 
+    <td>5.3</td>
+  </tr>
+  <tr>
+    <td>MySQL Version</td>
+    <td>2.2</td>
+  </tr>
+</table>
 </body>
 </html>
