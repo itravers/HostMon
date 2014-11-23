e2cb56c65786a738c16788d5dbb513103c5ebb89
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-22 16:29:50 -0800

    document ready added to menu.js

--- js/menu.js
+++ js/menu.js
@@ -4,31 +4,29 @@
 var menuTimeout; // Will control if we are updating menu or not.
 var focusedMenuItemName = 'none';
 
-//Event Handler when a user clicks on the menu. Opens the menu.
-$('.menu').click(function() {
-	setMenuConfigInfo();
-	//menuTimeout = setTimeout(setMenuConfigInfo, 5000);
-	$('nav').addClass('open');
-	$('body').addClass('menu-open');
-	return false;
-});
-
-//Event Handler when a user clicks anywhere but the menu, when the menu is open. Closes the menu.
-$(".grid").click(function() {
-	clearTimeout(menuTimeout); // Remove the timer.
-	$('body').removeClass('menu-open');
-	$('nav').removeClass('open');
-});
-
-//We need to check the class of id stopStartButton.
-var buttonClass = $("#stopStartButton").attr('class');
-var newButtonText = (buttonClass == 'backendRunning' ? 'STOP' : 'START');
-$("#stopStartButton").text(newButtonText);
-$("#stopStartLabel").text(newButtonText + " Backend");
-
-//Event Handler called when document is first loaded. Intializes the update of the grid graphs.
+//Event Handler called when document is first loaded.
 $(document).ready(function() {
-	alert("document ready");
+	//Event Handler when a user clicks on the menu. Opens the menu.
+	$('.menu').click(function() {
+		setMenuConfigInfo();
+		//menuTimeout = setTimeout(setMenuConfigInfo, 5000);
+		$('nav').addClass('open');
+		$('body').addClass('menu-open');
+		return false;
+	});
+
+	//Event Handler when a user clicks anywhere but the menu, when the menu is open. Closes the menu.
+	$(".grid").click(function() {
+		clearTimeout(menuTimeout); // Remove the timer.
+		$('body').removeClass('menu-open');
+		$('nav').removeClass('open');
+	});
+	
+	//We need to check the class of id stopStartButton.
+	var buttonClass = $("#stopStartButton").attr('class');
+	var newButtonText = (buttonClass == 'backendRunning' ? 'STOP' : 'START');
+	$("#stopStartButton").text(newButtonText);
+	$("#stopStartLabel").text(newButtonText + " Backend");
 });	
 
 
