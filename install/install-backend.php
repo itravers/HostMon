9257bfeddfd83593ad91d3f9b1930dede42febd8
Isaac Assegai <isaac.a.travers@gmail.com>
2014-11-26 11:54:18 -0800

    set error changes

--- install/install-backend.php
+++ install/install-backend.php
@@ -3,14 +3,33 @@ include_once("../php/db.php");
 if(isset($_POST['install'])){
 	$ajaxReturnVal = array();
 	if(install_testDB()){
-		$ajaxReturnVal['success'] = 'true';
+		recordDBSettings();
+		if(setupAdminAccount()){
+			$ajaxReturnVal['success'] = 'true';
+			$ajaxReturnVal['errorMessage'] = 'Install A Success.';
+		}else{
+			$ajaxReturnVal['success'] = 'false';
+			$ajaxReturnVal['errorMessage'] = 'Problem Setting up Admin Account.';
+		}
 	}else{
 		$ajaxReturnVal['success'] = 'false';
+		$ajaxReturnVal['errorMessage'] = 'Failed to Connect to db with those settings.';
 	}
 	$ajaxReturnVal = json_encode($ajaxReturnVal);
 	echo $ajaxReturnVal;
 }
 
+/** checks to see if the admin account is already set up, otherwise sets it up. */
+function setupAdminAccount(){
+	
+}
+
+/** Will record the db settings into cfg/db.cfg */
+function recordDBSettings(){
+	
+}
+
+/** Tests if the db is installed and available with the supplied credentials. */
 function install_testDB(){
 	$returnVal = false;
 	$con = mysqli_connect($_POST['dbAddress'], $_POST['dbUsername'], $_POST['dbPassword'], $_POST['dbName']);
@@ -19,6 +38,7 @@ function install_testDB(){
 	}else{
 		$returnVal = true;
 	}
+	
 	return $returnVal;
 }
 ?>
